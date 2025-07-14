2a986e55b96bdb17c0bf2e8519dc1cca57e9dcae
juanma chabaudie <jumachabaudie@gmail.com>
2025-07-14 12:35:47 -0300

    portfolio in progress

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Page = ({}) => {
+  return (
+    <div>
+      <h1>Hola</h1>
+    </div>
+  );
+};
+
+export default Page;
---
+++ my-app/tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      // Aquí puedes extender el tema si necesitas
+    },
+  },
+  plugins: [],
+};
+
+export default config;
